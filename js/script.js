8cce1fb43cc699bd2ffaae5806ff68aedf0d1dff
Mary Ilda Antonio Ramos <nosabiaqueponerdenombreflipa@gmail.com>
2026-03-08 00:39:37 +0100

    Menu hamburguesa/Modo Oscuro

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // FRANCO
+
+// MENÚ HAMBURGUESA
+const menu_icon = document.querySelector(".material-symbols-outlined");
+        menu_icon.addEventListener("click", () => {
+            const menu_nav = document.querySelector("nav");
+            menu_nav.classList.toggle("visible");
+        });
+
+
+// MODO OSCURO 
+const temaGuardado = localStorage.getItem("tema");
+
+        if (temaGuardado === "oscuro") {
+            document.body.classList.add("modo-oscuro");
+        }
+
+        const modo = document.querySelector("#btn-modo");
+
+        modo.addEventListener("click", () => {
+            const body = document.querySelector("body");
+            body.classList.toggle("modo-oscuro");
+            if (document.body.classList.contains("modo-oscuro")) {
+                localStorage.setItem("tema", "oscuro");
+            } else {
+                localStorage.setItem("tema", "claro");
+            }
+        })
