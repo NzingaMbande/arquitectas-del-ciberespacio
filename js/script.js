0fc42982e8421f45cddbc5bc1d43ba193ebeb73a
Nzinga <nosabiaqueponerdenombreflipa@gmail.com>
2026-03-08 03:41:54 +0100

    Merge Diana

--- js/script.js
+++ js/script.js
@@ -9,20 +9,42 @@ const menu_icon = document.querySelector(".material-symbols-outlined");
 
 
 // MODO OSCURO 
-const temaGuardado = localStorage.getItem("tema");
+// const temaGuardado = localStorage.getItem("tema");
 
-        if (temaGuardado === "oscuro") {
-            document.body.classList.add("modo-oscuro");
-        }
+//         if (temaGuardado === "oscuro") {
+//             document.body.classList.add("modo-oscuro");
+//         }
 
-        const modo = document.querySelector("#btn-modo");
+//         const modo = document.querySelector("#btn-modo");
 
-        modo.addEventListener("click", () => {
-            const body = document.querySelector("body");
-            body.classList.toggle("modo-oscuro");
-            if (document.body.classList.contains("modo-oscuro")) {
-                localStorage.setItem("tema", "oscuro");
-            } else {
-                localStorage.setItem("tema", "claro");
-            }
-        })
+//         modo.addEventListener("click", () => {
+//             const body = document.querySelector("body");
+//             body.classList.toggle("modo-oscuro");
+//             if (document.body.classList.contains("modo-oscuro")) {
+//                 localStorage.setItem("tema", "oscuro");
+//             } else {
+//                 localStorage.setItem("tema", "claro");
+//             }
+//         })
+
+
+// DIANA
+const titulo = document.getElementById("titulo");
+const texto = titulo.textContent;
+
+titulo.textContent = ""; // vaciar el h1
+
+texto.split("").forEach((letra, i) => {
+    const span = document.createElement("span");
+
+    if (letra === " ") {
+        // Si es un espacio, ponemos un &nbsp; sin animación
+        span.innerHTML = "&nbsp;";
+    } else {
+        span.textContent = letra;
+        span.classList.add("letra");
+        span.style.animationDelay = (i * 0.03) + "s";
+    }
+
+    titulo.appendChild(span);
+});
