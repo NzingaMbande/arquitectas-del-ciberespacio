d2392bf8811fec259321206b23b574e4fa3336b4
Nzinga <nosabiaqueponerdenombreflipa@gmail.com>
2026-03-13 01:25:14 +0100

    Update main

--- js/script.js
+++ js/script.js
@@ -51,44 +51,25 @@ menu_icon.addEventListener("click", () => {
 });
 
 // MODO OSCURO
-// const temaGuardado = localStorage.getItem("tema");
+const temaGuardado = localStorage.getItem("tema");
 
-//         if (temaGuardado === "oscuro") {
-//             document.body.classList.add("modo-oscuro");
-//         }
+        if (temaGuardado === "oscuro") {
+            document.body.classList.add("modo-oscuro");
+        }
 
-//         const modo = document.querySelector("#btn-modo");
+        const modo = document.querySelector("#btn-modo");
 
-//         modo.addEventListener("click", () => {
-//             const body = document.querySelector("body");
-//             body.classList.toggle("modo-oscuro");
-//             if (document.body.classList.contains("modo-oscuro")) {
-//                 localStorage.setItem("tema", "oscuro");
-//             } else {
-//                 localStorage.setItem("tema", "claro");
-//             }
-//         })
+        modo.addEventListener("click", () => {
+            const body = document.querySelector("body");
+            body.classList.toggle("modo-oscuro");
+            if (document.body.classList.contains("modo-oscuro")) {
+                localStorage.setItem("tema", "oscuro");
+            } else {
+                localStorage.setItem("tema", "claro");
+            }
+        })
 
-// DIANA
-// const titulo = document.getElementById("titulo");
-// const texto = titulo.textContent;
 
-// titulo.textContent = ""; // vaciar el h1
-
-// texto.split("").forEach((letra, i) => {
-//   const span = document.createElement("span");
-
-//   if (letra === " ") {
-//     // Si es un espacio, ponemos un &nbsp; sin animación
-//     span.innerHTML = "&nbsp;";
-//   } else {
-//     span.textContent = letra;
-//     span.classList.add("letra");
-//     span.style.animationDelay = i * 0.03 + "s";
-//   }
-
-//   titulo.appendChild(span);
-// });
 
 // Flip card mujeres.html
 const botones = document.querySelectorAll(".card_mujeres .voltear");
